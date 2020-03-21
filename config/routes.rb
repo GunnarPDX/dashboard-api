1186ee585ed8c9cba8da14345a9cca950fb3566b
Gunnar R <gunnarrosenberg@gmail.com>
2020-03-20 19:57:35 -0700

    add api route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      resources :data, only: %i[index]
+
     end
   end
 
-  get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
-    !request.xhr? && request.format.html?
+  get '*path', to: 'application#fallback_index_html', constraints: lambda {
+      |request| !request.xhr? && request.format.html?
   }
 end
